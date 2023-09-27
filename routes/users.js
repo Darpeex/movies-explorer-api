91f9515238e02ed24a178f1ec6d9c637b2fed84c
Darpeex <foxyyysp@mail.ru>
2023-09-27 09:02:06 +0300

    feat: добавлены роуты

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const { celebrate, Joi } = require('celebrate'); // библиотека для валидации данных
+const router = require('express').Router(); // создание нового экземпляра маршрутизатора вместо app
+
+const {
+  getUserInfo, updateUserInfo,
+} = require('../controllers/users');
+
+router.get('/users/me', getUserInfo); // возвращает информацию о пользователе (email и имя)
+
+router.patch( // обновляет информацию о пользователе (email и имя)
+  '/users/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      email: Joi.string().email().required(),
+    }).options({ abortEarly: false }), // проверяет все поля, даже если есть ошибка в одном из них
+  }),
+  updateUserInfo,
+);
+
+module.exports = router;
