0bbe6bc70e1772ab1645f4c61c8381150be1f724
Darpeex <92158213+Darpeex@users.noreply.github.com>
2023-09-28 18:42:02 +0300

    Revert "Level 1"

--- errors/owner-err.js
+++
@@ -1,9 +0,0 @@
-// экземпляр класса - ошибка фильм не ваш
-class OwnerCardError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 403;
-  }
-}
-
-module.exports = OwnerCardError;
